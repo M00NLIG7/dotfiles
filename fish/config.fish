4929e094d569b3daabad1e55fa9fbe9a295821a7
M00NLIG7 <cmagana2003@gmail.com>
2022-10-07 09:59:35 -0700

    added files

---
+++ fish/config.fish
@@ -0,0 +1,4 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+	alias kali="docker-compose -f /home/cm03/kali-light/docker-compose.yml run --rm kali-light"
+end
